7b055e61b3579fca78909e36892965730d191843
GarrettJenkinsonMayo <Jenkinson.William@mayo.edu>
2022-03-14 14:20:45 -0500

    make OS non-specific in paths

--- inst/unitTests/test_borealis.R
+++ inst/unitTests/test_borealis.R
@@ -1,7 +1,7 @@
 test_borealis <- function(){
     extdata <- system.file("extdata", package="borealis")
-    df <- runSingleNewSample(paste0(extdata,'/bismark/pat1/',
+    df <- runSingleNewSample(file.path(extdata,'bismark','pat1',
         'pat1_merged.cov.gz.CpG_report.merged_CpG_evidence.cov.gz'),
-        NULL,modelFile=paste0(extdata,'/CpG_model_chr1.csv'))
+        NULL,modelFile=file.path(extdata,'CpG_model_chr1.csv'))
     checkTrue(df$pVal[1]<0.05)
 }
