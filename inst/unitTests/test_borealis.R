7ac725187d7b5c6f44686522b5f7792ac89e0d0a
GarrettJenkinsonMayo <Jenkinson.William@mayo.edu>
2022-04-01 11:40:57 -0500

    make vignette one example; update docs to new extdata

--- inst/unitTests/test_borealis.R
+++ inst/unitTests/test_borealis.R
@@ -1,7 +1,8 @@
 test_borealis <- function(){
     extdata <- system.file("extdata", package="borealis")
-    df <- runSingleNewSample(file.path(extdata,'bismark','pat1',
-        'pat1_merged.cov.gz.CpG_report.merged_CpG_evidence.cov.gz'),
-        NULL,modelFile=file.path(extdata,'CpG_model_chr1.csv'))
-    checkTrue(df$pVal[1]<0.05)
+    df <- runSingleNewSample(file.path(extdata,'bismark','patient_72',
+                                        'patient_72.gz'),
+                        NULL,modelFile=file.path(extdata,'CpG_model_chr14.csv'))
+    pVal <- df$pVal[df$start=="24780288"]
+    checkTrue(pVal<0.05)
 }
