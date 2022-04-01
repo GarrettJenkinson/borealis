e431d0b36c73c9a91650bf21320e3691f1a46a92
GarrettJenkinsonMayo <Jenkinson.William@mayo.edu>
2022-04-01 12:36:02 -0500

    fix errors/biocCheck complaints

--- inst/unitTests/test_borealis.R
+++ inst/unitTests/test_borealis.R
@@ -3,6 +3,6 @@ test_borealis <- function(){
     df <- runSingleNewSample(file.path(extdata,'bismark','patient_72',
                                         'patient_72.gz'),
                         NULL,modelFile=file.path(extdata,'CpG_model_chr14.csv'))
-    pVal <- df$pVal[df$start=="24780288"]
+    pVal <- df$pVal[df$pos == "24780288"]
     checkTrue(pVal<0.05)
 }
